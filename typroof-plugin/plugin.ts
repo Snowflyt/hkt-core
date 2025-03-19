360301531330c7d7384b86972c7bffc4bc6d95a8
Snowflyt <gaoge011022@163.com>
2025-03-19 14:56:19 +0800

    🧪 test: Migrate to Typroof v0.5

--- typroof-plugin/plugin.ts
+++ typroof-plugin/plugin.ts
@@ -1,5 +1,6 @@
-import chalk from "chalk";
+import type * as ts from "typescript";
 import type { Plugin } from "typroof/plugin";
+import { bold } from "typroof/utils/colors";
 
 /**
  * The hkt-core plugin for Typroof.
@@ -9,41 +10,46 @@ const hktCore = (): Plugin => ({
   name: "typroof-plugin-hkt-core",
 
   analyzers: {
-    beOfSig(actual, expected, { not, validationResult }) {
-      const actualText = chalk.bold(actual.text);
-      const actualSignature = chalk.bold(validationResult!.getTupleElements()[0]!.getText());
-      const expectedSignature = chalk.bold(expected.getText());
+    beOfSig(actual, expected, { not, typeChecker, validationResult }) {
+      const actualText = bold(actual.text);
+      const expectedSignature = bold(typeChecker.typeToString(expected));
 
-      if (
-        validationResult!.getTupleElements()[0]!.getText() !==
-        validationResult!.getTupleElements()[1]!.getText()
-      )
+      if (!validationResult)
         if (not)
           throw `Expect TypeLambda ${actualText} not to be of signature ${expectedSignature}, but does.`;
         else
-          throw `Expect TypeLambda ${actualText} to be of signature ${expectedSignature}, but got ${chalk.bold(
+          throw `Expect TypeLambda ${actualText} to be of signature ${expectedSignature}, but does not.`;
+
+      const tupleTypes = typeChecker.getTypeArguments(validationResult as ts.TupleType);
+      const actualSignature = bold(typeChecker.typeToString(tupleTypes[0]!));
+
+      if (typeChecker.typeToString(tupleTypes[0]!) !== typeChecker.typeToString(tupleTypes[1]!))
+        if (not)
+          throw `Expect TypeLambda ${actualText} not to be of signature ${expectedSignature}, but does.`;
+        else
+          throw `Expect TypeLambda ${actualText} to be of signature ${expectedSignature}, but got ${bold(
             actualSignature,
           )}.`;
     },
 
-    beOneOf(actual, expected, { not }) {
-      const actualText = chalk.bold(actual.text);
-      const expectedTypes = chalk.bold(expected.getText().slice(1, -1));
+    beOneOf(actual, expected, { not, typeChecker }) {
+      const actualText = bold(actual.text);
+      const expectedTypes = bold(typeChecker.typeToString(expected).slice(1, -1));
 
       throw `Expect type ${actualText} ${not ? "not " : ""}to be one of ${expectedTypes}, but is${
         not ? "" : " not"
       }.`;
     },
 
-    exactEqual(actual, expected, { not }) {
-      if (actual.type.getText() === expected.getText()) return;
+    exactEqual(actual, expected, { not, typeChecker }) {
+      if (typeChecker.typeToString(actual.type) === typeChecker.typeToString(expected)) return;
 
-      const actualText = chalk.bold(actual.text);
-      const expectedType = chalk.bold(expected.getText());
+      const actualText = bold(actual.text);
+      const expectedType = bold(typeChecker.typeToString(expected));
 
       let message = `Expect ${actualText}`;
-      if (actual.text !== actual.type.getText())
-        message += ` (${chalk.bold(actual.type.getText())})`;
+      if (actual.text !== typeChecker.typeToString(actual.type))
+        message += ` (${bold(typeChecker.typeToString(actual.type))})`;
       if (not) message += " not";
       message += ` to exactly equal ${expectedType}, but does`;
       if (!not) message += " not";
